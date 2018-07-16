a69a6b88f5515f1c9098444e9914d79d7dbf592f
Cynthia Foxwell <lumodder@gmail.com>
2018-07-15 21:29:31 -0600

    not all playlists are full 32 length

--- cmds/music.js
+++ cmds/music.js
@@ -4,7 +4,7 @@ const probe = require("node-ffprobe");
 
 const ytregex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
 const plregex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/playlist\?list=(.+)$/;
-const plregex2 = /^PL[a-zA-Z0-9-_]{32}$/;
+const plregex2 = /^PL[a-zA-Z0-9-_]{1,32}$/;
 const mp3regex = /^(https?:\/\/)?.*\..*\/.+\.(mp3|ogg|flac|wav)$/;
 const scregex = /^(https?:\/\/)?(www\.|m\.)?soundcloud\.com\/.+\/.+$/;
 const scregex2 = /^sc:.+\/.+$/;
