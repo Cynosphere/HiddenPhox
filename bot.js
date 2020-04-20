1ba4b98952771a670e83f83aff9a1bc3b797b400
Cynthia Foxwell <lumodder@gmail.com>
2020-04-20 14:33:12 -0600

    test non special intents

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ const client = new Eris(config.token, {
     defaultImageSize: 1024,
     intents: [
         "GUILDS",
-        "GUILD_MEMBERS",
+        //"GUILD_MEMBERS",
         "GUILD_BANS",
         "GUILD_EMOJIS",
         "GUILD_VOICE_STATES",
-        "GUILD_PRESENCES",
+        //"GUILD_PRESENCES",
         "GUILD_MESSAGES",
         "GUILD_MESSAGE_REACTIONS",
         "DIRECT_MESSAGES",
